191a085e13d8c1265b4bf35a6941ca68624b5435
npkuuga060 <wachirawit_porn@cmu.ac.th>
2018-05-10 23:59:18 +0700

    Edit follwing

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -1,17 +1,45 @@
 'use strict';
 
 angular.module('followingList', ['ngRoute'])
-  .component('followingList', {
-    templateUrl: 'following/following.html',
-    controller: ['$http', '$rootScope', function TweetListController($http, $rootScope) {
-      var self = this;
+    .component('followingList', {
+        templateUrl: 'following/following.html',
+        controller: ['$http', '$rootScope', function TweetListController($http, $rootScope) {
+            var self = this;
+            const requestOptions = {
+                headers: { 'X-session': $rootScope.x_session }
+            };
 
-      const requestOptions = {
-          headers: { 'X-session': $rootScope.x_session }
-      };
+            self.sendfollowing = function sendfollowing(followingname) {
+                const requestOptions = {
+                    headers: { 'X-session': $rootScope.x_session }
+                };
+                var data = "followingname=" + encodeURIComponent(followingname);
+                $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+                    $http.get('http://localhost:8080/twitterapi/following ', requestOptions).then(function (response) {
+                        self.tweets = response.data;
+                        self.tweets.forEach(function iterator(value, index, collection) {
+                            value.Message = decodeURIComponent(value.Message);
+                        });
+                    });
+                });
+            }
 
-      $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
-        self.followings = response.data;
-      });
-    }]
-});
+            self.deletefollowing = function deletefollowing(followingname) {
+                const requestOptions = {
+                    headers: { 'X-session': $rootScope.x_session }
+                };
+                var data = "followingname=" + encodeURIComponent(followingname);
+                $http.delete('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+                    self.followings = response.data;
+
+                });
+
+
+            }
+
+            $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
+                self.followings = response.data;
+
+            });
+        }]
+    });
